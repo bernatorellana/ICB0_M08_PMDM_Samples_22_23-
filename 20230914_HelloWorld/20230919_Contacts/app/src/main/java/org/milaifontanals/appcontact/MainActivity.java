c8075316252500f085cc2ce10bf9f9384e69075d
Bernat <bernat.orellana@gmail.com>
2023-09-21 17:30:38 +0200

    dades¡

--- 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
+++ 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
@@ -3,12 +3,36 @@ package org.milaifontanals.appcontact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import org.milaifontanals.appcontact.databinding.ActivityMainBinding;
+import org.milaifontanals.appcontact.model.Contact;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+/*
+    private EditText edtNewPhone;
+    private ImageButton imbAdd;
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mostrarContacte(Contact.getContactes().get(1));
+
+    }
+
+    private void mostrarContacte(Contact c) {
+        binding.llyFitxaDades.imvPhoto.setImageResource(c.getImatge());
+        binding.llyFitxaDades.edtName.setText(c.getNom());
+        binding.llyFitxaDades.edtEmail.setText(c.getEmail());
     }
 }
