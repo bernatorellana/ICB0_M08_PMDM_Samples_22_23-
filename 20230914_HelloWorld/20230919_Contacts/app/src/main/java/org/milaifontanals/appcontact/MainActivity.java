4a5cc7e67250ad476044f216102ee1f3f5f8a3a0
Bernat <bernat.orellana@gmail.com>
2023-09-25 20:02:07 +0200

    botons fets

--- 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
+++ 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
@@ -3,12 +3,15 @@ package org.milaifontanals.appcontact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import org.milaifontanals.appcontact.databinding.ActivityMainBinding;
 import org.milaifontanals.appcontact.model.Contact;
+import org.milaifontanals.appcontact.utils.TextChangedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,13 +31,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         mostrarContacte(indexContacte);
-
+        //******************************************************************
+        // Programació d'events
+        //******************************************************************
         // Programar els botons
-        binding.btnForward.setOnClickListener(view1 -> {
-            indexContacte  =(indexContacte+1)%Contact.getContactes().size();
+        binding.btnForward.setOnClickListener( c -> {
+            // Codi que s'executa quan fas click sobre el botó forward
+            //indexContacte  =(indexContacte+1)%Contact.getContactes().size();
+            if(indexContacte<Contact.getContactes().size()-1){
+                indexContacte++;
+            }
             mostrarContacte(indexContacte);
         });
 
+        binding.btnBack.setOnClickListener( c -> {
+            // Codi que s'executa quan fas click sobre el botó back
+            if(indexContacte>0){
+                indexContacte--;
+            }
+            mostrarContacte(indexContacte);
+        });
+        // Programar els textchanged
+        binding.llyFitxaDades.edtName.addTextChangedListener(new TextChangedListener() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //*************************************************************
 
     }
 
@@ -43,5 +68,11 @@ public class MainActivity extends AppCompatActivity {
         binding.llyFitxaDades.imvPhoto.setImageResource(c.getImatge());
         binding.llyFitxaDades.edtName.setText(c.getNom());
         binding.llyFitxaDades.edtEmail.setText(c.getEmail());
+
+        binding.btnBack.setEnabled(index>0);
+        binding.btnBack.setAlpha(binding.btnBack.isEnabled()?1: 0.25f);
+        binding.btnForward.setEnabled(index<Contact.getContactes().size()-1);
+        binding.btnForward.setAlpha(binding.btnForward.isEnabled()?1: 0.25f);
+
     }
 }
---
+++ 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/utils/TextChangedListener.java
@@ -0,0 +1,17 @@
+package org.milaifontanals.appcontact.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public abstract class TextChangedListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+}
