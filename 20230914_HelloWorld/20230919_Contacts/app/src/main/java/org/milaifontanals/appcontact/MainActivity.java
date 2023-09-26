ede1cd4fc4180cb54bdf2a09bfc42a3cb4e392d7
Bernat <bernat.orellana@gmail.com>
2023-09-26 17:21:05 +0200

    desant el nom

--- 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
+++ 20230914_HelloWorld/20230919_Contacts/app/src/main/java/org/milaifontanals/appcontact/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -16,6 +17,7 @@ import org.milaifontanals.appcontact.utils.TextChangedListener;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "AGENDA_CONTACTES";
     private ActivityMainBinding binding;
 /*
     private EditText edtNewPhone;
@@ -55,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
         binding.llyFitxaDades.edtName.addTextChangedListener(new TextChangedListener() {
             @Override
             public void afterTextChanged(Editable editable) {
-
+                Log.d(TAG, "estic passant pel textChanged"+binding.llyFitxaDades.edtName.getText());
+                Contact actual = Contact.getContactes().get(indexContacte);
+                actual.setNom(binding.llyFitxaDades.edtName.getText().toString());
             }
         });
 
