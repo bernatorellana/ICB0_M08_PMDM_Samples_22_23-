d1dcf2ddd93c69df894195eaa7473393a1e84722
Bernat <bernat.orellana@gmail.com>
2023-10-05 17:48:06 +0200

    opció seleccionad

--- 20231003_RecyclerView/app/src/main/java/org/milaifontanals/recyclerviewapp/adapters/CardsAdapter.java
+++ 20231003_RecyclerView/app/src/main/java/org/milaifontanals/recyclerviewapp/adapters/CardsAdapter.java
@@ -1,6 +1,7 @@
 package org.milaifontanals.recyclerviewapp.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
@@ -23,6 +24,9 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder>{
     private List<Card> mCards;
     private Context mContext;
 
+    private int idxElementSeccionat = -1;
+
+
     public CardsAdapter(Context c, List<Card> cards){
         mContext = c;
         mCards = cards;
@@ -43,6 +47,15 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder>{
         rowHolder.txvDesc.setText(cartaActual.getDesc());
         rowHolder.txvCost.setText(""+cartaActual.getElixirCost());
         rowHolder.imvPhoto.setImageResource(cartaActual.getDrawable());
+
+
+        rowHolder.itemView.setOnClickListener(view -> {
+            int posicioAnterior = this.idxElementSeccionat;
+            this.idxElementSeccionat = rowHolder.getAdapterPosition();
+            this.notifyItemChanged(idxElementSeccionat);
+            this.notifyItemChanged(posicioAnterior);
+        });
+
         int color;
         switch (cartaActual.getRarity()){
             case EPIC: color =  mContext.getColor(R.color.RARITY_EPIC);break;
@@ -56,9 +69,15 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder>{
                     GradientDrawable.Orientation.BOTTOM_TOP,
                     new int[]{
                         Color.parseColor("#ffffff"), // color inici
-                        color // color fi
-        }));
-        //rowHolder.clyRow.setBackgroundColor(color);
+                            color,
+                            color // color fi
+                    }
+        ));
+        if(position==idxElementSeccionat) {
+            rowHolder.clyRow.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ffff00")));
+        } else {
+            rowHolder.clyRow.setBackgroundTintList(null);
+        }
     }
 
     @Override
