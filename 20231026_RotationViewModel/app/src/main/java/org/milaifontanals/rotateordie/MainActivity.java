44630cc4c39d5d3290595eb465373a9728da7e09
developrepogithub <develop.repo.github@gmail.com>
2023-10-31 16:56:19 +0100

    Exemple amb fil i MutableLiveData

--- 20231026_RotationViewModel/app/src/main/java/org/milaifontanals/rotateordie/MainActivity.java
+++ 20231026_RotationViewModel/app/src/main/java/org/milaifontanals/rotateordie/MainActivity.java
@@ -9,8 +9,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.TextView;
 
 import org.milaifontanals.rotateordie.databinding.ActivityMainBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     public static final String SCORE_HOME = "SCORE_HOME";
@@ -26,9 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
+        // Instanciem el ViewModel amb totes les seves dades
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         //viewModel = new MainActivityViewModel();// Facil....però ERRRRRRRRONI !!!!!
-
+        viewModel.getHoraActual().observe(this,date -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+            binding.txvTime.setText( sdf.format(date));
+        });
 
         //setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -42,6 +52,23 @@ public class MainActivity extends AppCompatActivity {
             viewModel.addScoreHome(-1);     showScore();return true;});
         binding.btnVisitorUp.setOnLongClickListener(view -> {
             viewModel.addScoreVisitor(-1);  showScore();return true;});
+
+
+
+        /*Thread t = new Thread(() -> {
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Date d = new Date();
+                //binding.txvTime.setText(d.getMinutes()+":"+d.getSeconds());
+                ((TextView)findViewById(R.id.txvTime)).setText(d.getMinutes()+":"+d.getSeconds());
+            }
+        });
+        t.start();
+        */
     }
 
 
--- 20231026_RotationViewModel/app/src/main/java/org/milaifontanals/rotateordie/MainActivityViewModel.java
+++ 20231026_RotationViewModel/app/src/main/java/org/milaifontanals/rotateordie/MainActivityViewModel.java
@@ -1,11 +1,44 @@
 package org.milaifontanals.rotateordie;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivityViewModel extends ViewModel {
 
     private int scoreHome=0;
     private int scoreVisitor=0;
+
+    private Date horaInicial;
+    private MutableLiveData<Date> horaActual = new MutableLiveData<>();
+
+    public MainActivityViewModel(){
+        horaInicial = new Date();
+        horaActual.setValue(horaInicial);
+        programarTimer();
+
+    }
+
+    private void programarTimer() {
+
+       Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                long milisegons = (new Date()).getTime() - horaInicial.getTime();
+                Date diff = new Date(milisegons);
+                horaActual.postValue(diff);
+            }
+        },0,1000);
+    }
+
+
+    public MutableLiveData<Date> getHoraActual() {
+        return horaActual;
+    }
 
     public int getScoreHome() {
         return scoreHome;
